98cc7d99cb04e7e95854ffa21abe620090f64df5
kpasan89 <kpasan89@gmail.com>
2017-05-04 10:00:25 +0530

    Signed-off-by: kpasan89 <kpasan89@gmail.com>

--- target/generated-sources/annotations/com/pcmis/entity/Country_.java
+++ target/generated-sources/annotations/com/pcmis/entity/Country_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-03T15:51:54")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-04T09:50:38")
 @StaticMetamodel(Country.class)
 public class Country_ { 
 
