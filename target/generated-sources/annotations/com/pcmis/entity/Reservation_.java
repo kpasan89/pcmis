bd6ac0c78af2932e8240fbb3c12122a42446c930
kpasan89 <kpasan89@gmail.com>
2017-06-11 12:26:39 +0530

    Signed-off-by: kpasan89 <kpasan89@gmail.com>

--- target/generated-sources/annotations/com/pcmis/entity/Reservation_.java
+++ target/generated-sources/annotations/com/pcmis/entity/Reservation_.java
@@ -10,7 +10,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-11T11:52:07")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-11T12:24:59")
 @StaticMetamodel(Reservation.class)
 public class Reservation_ { 
 
