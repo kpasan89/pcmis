9d2892a64e4658e2923aa381413967f761f9a390
kpasan89 <kpasan89@gmail.com>
2017-06-12 08:56:55 +0530

    Signed-off-by: kpasan89 <kpasan89@gmail.com>

--- target/generated-sources/annotations/com/pcmis/entity/UserRole_.java
+++ target/generated-sources/annotations/com/pcmis/entity/UserRole_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-11T12:24:59")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-11T18:46:07")
 @StaticMetamodel(UserRole.class)
 public class UserRole_ { 
 
